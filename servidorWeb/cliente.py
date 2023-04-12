4ff245bee0df77f7f3e15a8f48b82c61b44137f0
Matheus Augusto <maabonfim2003@gmail.com>
2023-04-12 12:37:26 -0300

    Correções na comunicação cliente/servidor

--- servidorWeb/cliente.py
+++ servidorWeb/cliente.py
@@ -8,7 +8,10 @@ socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # AFINET = IP // socket_stream = tcp ===> tcp/ip
 
 # conecta no servidor
-socket_client.bind(('localhost', 8000))
+socket_client.connect(('localhost', 8000))
+
+# output
+print(f'conexao deu certo.')
 
 # envia ping para o server
 socket_client.sendall(b'ping!')
@@ -18,9 +21,6 @@ dado = socket_client.recv(1024)
 dado = dado.decode()
 # output
 print(f'recebido do servidor: {dado}')
-
-# output
-print(f'conexao deu certo.')
 
 # finaliza a conexao
 socket_client.close()
