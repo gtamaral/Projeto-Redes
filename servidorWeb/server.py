c92d878421b3abb0f35310c66803aa0022138f56
gtamaral <rodrigoamaral003@gmail.com>
2023-04-12 22:44:39 -0300

    correcoes de alguns bugs e adicionando arqvs dinamicos

--- servidorWeb/server.py
+++ servidorWeb/server.py
@@ -4,6 +4,9 @@ import sys
 enderecoServer = '0.0.0.0'
 portaServer = 8000
 
+type_arquivoBinario = ['png', 'jpeg', 'bmp']
+type_arquivoTexto = ['html', 'css', 'js']
+
 socket_servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # cria um objeto socket e passa como para parametro 02 argumentos
 # AFINET = IP // socket_stream = tcp ===> tcp/ip
@@ -12,48 +15,61 @@ socket_servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_servidor.bind((enderecoServer, portaServer))    # o primeiro argumento serve para ouvir em todas as placas de redes disponiveis
 socket_servidor.listen()
 
-# aguardo uma conexao com o client
-print(f'servidor ouvindo em {enderecoServer} : {portaServer} pronto para receber as conexoes')    # ouput
-socket_client, client_addr = socket_servidor.accept()
-# a funcao accept retorna o endereco ipv4 e a porta de origigem do mesmo
+while True:
+    # aguardo uma conexao com o client
+    print(f'servidor ouvindo em {enderecoServer} : {portaServer} pronto para receber as conexoes')    # ouput
+    socket_client, client_addr = socket_servidor.accept()
+    # a funcao accept retorna o endereco ipv4 e a porta de origigem do mesmo
 
-# output
-print(f'cliente se conextou com exito. {client_addr[0]}: {client_addr[1]}')
+    # output
+    print(f'cliente se conextou com exito. {client_addr[0]}: {client_addr[1]}')
 
-# receber dados do cliente
-dadoRecebido = socket_client.recv(1024)     #recebe ate 1024btes
-dadoRecebido = dadoRecebido.decode()        #decodifica o byte p str
+    # receber dados do cliente
+    dadoRecebido = socket_client.recv(1024)     #recebe ate 1024btes
+    dadoRecebido = dadoRecebido.decode()        #decodifica o byte p str
 
-# parcing do cabeçalho
-headers = dadoRecebido.split('\r\n')
-header_get = headers[0] # isolando a primeira linha do cabeçalho
+    # parcing do cabeçalho
+    headers = dadoRecebido.split('\r\n')
+    header_get = headers[0] # isolando a primeira linha do cabeçalho
 
-# obtendo o arquivo solicitado
-arquivo_solicitado = header_get.split(' ') [1] [1:] # transformando a primeira linha em um array, pegando o segundo elemento e eliminando o primeiro caracter que seria "/"
-print (f'Arquivo solicitado: {arquivo_solicitado}')
+    # obtendo o arquivo solicitado
+    arquivo_solicitado = header_get.split(' ') [1] [1:] # transformando a primeira linha em um array, pegando o segundo elemento e eliminando o primeiro caracter que seria "/"
+    print (f'Arquivo solicitado: {arquivo_solicitado}')
 
-# abrir o arquivo
-try:
-    file = open(arquivo_solicitado, 'r', encoding = 'utf-8')    
-    conteudo_arquivo = file.read()
+    # obtendo extensao do arquivo
+    extensao = arquivo_solicitado.split('.')[-1]
+    
+    arquivoBinario = False
+    if extensao in type_arquivoBinario:
+        arquivoBinario = True
 
-# quando o arquivo nao é encontrado == error404
-except FileNotFoundError:
-    print(f'Arquivo não existe {arquivo_solicitado}')
-    socket_client.sendall(b'HTTP/1.1 404 File not found\r\n\r\nFile not found')
+    # abrir o arquivo
+    try:
+        if arquivoBinario:
+            file = open(arquivo_solicitado, 'rb')
+        else:
+            file = open(arquivo_solicitado, 'r', encoding = 'utf-8')    
+        conteudo_arquivo = file.read()
+
+    # quando o arquivo nao é encontrado == error404
+    except FileNotFoundError:
+        print(f'Arquivo não existe {arquivo_solicitado}')
+        socket_client.sendall(b'HTTP/1.1 404 File not found\r\n\r\nFile not found')
+        socket_client.close()
+        continue
+
+    # resposta ao browser
+    cabecalho_resposta = f'HTTP/1.1 200 OK\r\n\r\n'
+    corpo_resposta = conteudo_arquivo
+
+    if arquivoBinario:
+        resposta_final = bytes(cabecalho_resposta, 'utf-8') + corpo_resposta
+        socket_client.sendall(resposta_final)   #responde p o cliente
+    else:
+        resposta_final = cabecalho_resposta + corpo_resposta
+        socket_client.sendall(resposta_final.encode('utf-8'))   #responde p o cliente
+   
+    # encerrando a cnx
     socket_client.close()
-    sys.exit(1)
 
-
-# resposta ao browser
-cabecalho_resposta = f'HTTP/1.1 200 OK\r\n\r\n'
-corpo_resposta = conteudo_arquivo
-
-resposta_final = cabecalho_resposta + corpo_resposta
-
-# responde para o cliente
-socket_client.sendall(resposta_final.encode('utf-8'))   #codifica de str para bytes
-
-# encerrando a cnx
-socket_client.close()
-
+socket_servidor.close()
